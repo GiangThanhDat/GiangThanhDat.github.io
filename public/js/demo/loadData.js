576baefe334e49c9bd2bd61b71fec3c03e5cbddd
thanhdat <43725593+GiangThanhDat@users.noreply.github.com>
2020-08-08 19:22:55 +0700

    Update loadData.js

--- public/js/demo/loadData.js
+++ public/js/demo/loadData.js
@@ -1,6 +1,6 @@
 
-function browse(idStation,sensorMeasuresList) {
-	sensorMeasuresList.forEach(function(elem) {
+function browse(idStation,sensorMeasuresList) {	
+	sensorMeasuresList.forEach(function(elem) {		
 		setValue(idStation,elem['ma_cambien'],elem['ma_dailuong']);
 	});
 }
@@ -8,14 +8,23 @@ function browse(idStation,sensorMeasuresList) {
 function setValue(ma_tram,ma_cambien,ma_dailuong) {
 	$.get("collect/get/"+ma_tram+"/"+ma_cambien+"/"+ma_dailuong,function(val){
 		// console.log(val);
-		val = $.parseJSON(val);
-		// console.log(val);
+		val = $.parseJSON(val);		
 		value = val['val'];
 		time = val['time'];
-		var myTime = time.substr(11, 8);		
-		$('#val_'+ma_tram+"_"+ma_cambien+"_"+ma_dailuong).html(value);
-		$('#time_'+ma_tram+"_"+ma_cambien+"_"+ma_dailuong).html(myTime);
-		
+		max = val['max'];
+		min = val['min'];
+		myColor = val['mau'];
+		var myTime = time.substr(11, 8);
+		if (value > max) {
+			console.log(value + " > " + max);
+			// console.log($('#val_'+ma_tram+"_"+ma_cambien+"_"+ma_dailuong));
+			// $('#val_'+ma_tram+"_"+ma_cambien+"_"+ma_dailuong).css("background-color", );			
+			$('#val_'+ma_tram+"_"+ma_cambien+"_"+ma_dailuong).css("color", myColor);	
+			$('#time_'+ma_tram+"_"+ma_cambien+"_"+ma_dailuong).css("color", myColor);	
+			$('#dv_'+ma_tram+"_"+ma_cambien+"_"+ma_dailuong).css("color", myColor);	
+		}
+		$('#val_'+ma_tram+"_"+ma_cambien+"_"+ma_dailuong).html(value);		
+		$('#time_'+ma_tram+"_"+ma_cambien+"_"+ma_dailuong).html(myTime);		
 	});
 }
 
@@ -24,3 +33,4 @@ $(document).ready(function(){
 		browse(idStation,sensorMeasuresList);
 	}, 100);
 });
+
